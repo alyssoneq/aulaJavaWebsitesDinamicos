4d14b1aa4696798d9762e03924d7a2aad7a83f68
alyssoneq <alyssoneq@gmail.com>
2021-01-19 10:16:55 -0300

    Projeto básico com um servlet criado para imprimir na tela um texto básico. Além disso, um arquivo .txt com conceitos sobre o ciclo de vida de um servlet

---
+++ PraticandoServlet/src/todosServlets/MeuPrimeiroServlet.java
@@ -0,0 +1,63 @@
+package todosServlets;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MeuPrimeiroServlet
+ * 
+ * O '@' é utilizado para mapear a URL ao servlet
+ * A não utilização do '@' implica no mapeamento manual
+ * O mapeamento manual é feito no arquivo web.xml
+ */
+@WebServlet("/MeuPrimeiroServlet")
+public class MeuPrimeiroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MeuPrimeiroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String html = "<html><body><h1> Meu primeiro Servlet </h1></body></html>";
+		response.getWriter().append(html);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
