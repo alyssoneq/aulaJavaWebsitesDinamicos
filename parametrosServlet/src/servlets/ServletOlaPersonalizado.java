c41d6022c70d8e1f60c84b481a424c9eed057d92
alyssoneq <alyssoneq@gmail.com>
2021-01-19 19:33:05 -0300

    Projeto simples para aprender a trabalhar com manipulação de parametros em servlets

---
+++ parametrosServlet/src/servlets/ServletOlaPersonalizado.java
@@ -0,0 +1,46 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletOlaPersonalizado
+ * 
+ * Servlet criado para testar funcionalidade GET
+ * O método doGet() receberá parâmetros para processar
+ */
+@WebServlet("/ServletOlaPersonalizado")
+public class ServletOlaPersonalizado extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOlaPersonalizado() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Método com parâmetros recebido pela URL
+		String nome = request.getParameter("nome");
+		String idade = request.getParameter("idade");
+		
+		// Pequeno código html para inserir o nome na tela
+		String html = "<html><body><h1>" +
+						nome +
+						"<p> idade: "+
+						idade +
+						" anos </p></h1></body></html>";
+		response.getWriter().append(html);
+	}
+
+	
+}
