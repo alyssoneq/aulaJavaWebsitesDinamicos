8a62f00c645efaad4e71c093f4f897a1a96ad4ef
alyssoneq <alyssoneq@gmail.com>
2021-03-17 19:16:23 -0300

    Adicionando projeto simples para exercitar conexão com banco de dados. Inicialmente foi criado uma classe para realizar conexão com banco de dados.

---
+++ TesteConexaoBD/src/conexao/ConexaoBD.java
@@ -0,0 +1,33 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Classe para conectar com um banco de dados
+ * 
+ * */
+public class ConexaoBD {
+	
+	// Método de conexão com o banco de dados
+	public static Connection conecta(String serverName, String myDatabase, String userName, String password) throws ClassNotFoundException {
+		
+		Connection conexao = null;
+		
+		String driverName = "com.mysql.jdbc.driver";
+		Class.forName(driverName);
+		String url = "jdbc:mysql://" + serverName + "/" + myDatabase;
+		
+		try {
+			conexao = DriverManager.getConnection(url, userName, password);
+			System.out.println("conexão estabelecida com sucesso!");
+			
+		} catch (SQLException e) {
+			System.err.println("Erro na conexão...");
+		}
+		
+		return conexao;
+		
+	}
+}
---
+++ TesteConexaoBD/src/livros/ConsultaLivros.java
@@ -0,0 +1,12 @@
+package livros;
+
+public class ConsultaLivros {
+	
+	// Construtor
+	public ConsultaLivros() {	
+	}
+	
+	public void listaLivros() {
+		
+	}
+}
