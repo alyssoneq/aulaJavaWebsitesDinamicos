057b770ab4fc11e49735c4036c2a089f72a26219
alyssoneq <alyssoneq@gmail.com>
2021-03-17 21:41:19 -0300

    Criando classe para executar consulta básica da tabela livros do banco de dados livraria.

--- TesteConexaoBD/src/livros/ConsultaLivros.java
+++ TesteConexaoBD/src/livros/ConsultaLivros.java
@@ -1,12 +1,57 @@
 package livros;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import conexao.ConexaoBD;
+
 public class ConsultaLivros {
 	
 	// Construtor
 	public ConsultaLivros() {	
 	}
 	
-	public void listaLivros() {
+	// Método para listar id e nome de todos os livros do banco de dados
+	public void listaLivros() throws ClassNotFoundException {
 		
+		Statement statement = null;
+		String serverName = "localhost";
+		String myDatabase = "livraria";
+		String userName = "root";
+		String password = "********";
+		Connection conexao = ConexaoBD.conecta(serverName, myDatabase, userName, password);
+		
+		try {
+			statement = (Statement) conexao.createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		ResultSet result = null;
+		
+		try {
+			String query = "SELECT * FROM livros";
+			result = statement.executeQuery(query);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			System.out.println("------------------------------------------");
+			while (result.next()) {
+				System.out.println(result.getInt("idLivro") + ":  " + result.getString("titulo"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			result.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
