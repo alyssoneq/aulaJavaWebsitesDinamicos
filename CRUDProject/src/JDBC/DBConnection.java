e70105ada49a1aea589a60056298ed5ff68825e8
alyssoneq <alyssoneq@gmail.com>
2021-03-24 19:09:31 -0300

    Creating class to manage connections with the database for the users table

---
+++ CRUDProject/src/JDBC/DBConnection.java
@@ -0,0 +1,30 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/* *
+ * Class to manage the database connection
+ * 
+ * */
+
+public class DBConnection {
+
+	// Connection params
+	private static final String url = "jdbc:mysql://localhost/crud?useSSL=false";
+	private static final String userName = "root";
+	private static final String password = "*******";
+	
+	// Method to connect with the database
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(url, userName, password);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+}
